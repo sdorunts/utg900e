df4087002a1188383f10ae99ab5490cc52120360
Shagen Dorunts <shadorunts@sberautotech.ru>
2025-10-23 16:16:09 +0300

    [+] Files for building and PyPI sharing added. Gitignore supplemented

--- setup.py
+++ setup.py
@@ -3,15 +3,33 @@ from setuptools import setup, find_packages
 setup(
     name="utg900e",
     version="0.1.0",
-    description="Библиотека управления генераторами сигналов серии UTG900E через SCPI (VISA)",
+    description="A Python library for controlling UTG900E series signal generators via SCPI commands using VISA interface",
     author="Shagen Dorunts",
+    author_email="sdorunts@yandex.com",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
-        "pyvisa>=1.11.0"
+        "pyvisa>=1.11.0",
+        "colorlog>=6.9.0",
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.7",
+    project_urls={
+        "Homepage": "https://github.com/sdorunts/utg900e",
+        "Repository": "https://github.com/sdorunts/utg900e",
+    },
 )
--- utg900e/__init__.py
+++ utg900e/__init__.py
@@ -1,1 +1,5 @@
+
+__version__ = "0.1.0"
 from .utg900e import UTG900E
+
+__all__ = ["UTG900E"]
